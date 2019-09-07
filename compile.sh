adb292c7640b032c509486abec2705fd82569d22
Philipp Muggli <pmpmlpml@gmail.com>
2019-09-07 11:25:26 +0200

    Added typestripping
    Added automatic typestripping on compile for easier minification and a smaller code base.

--- compile.sh
+++ compile.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
+
+# strip typing hints, for easier minification
+strip-hints ./out/scrolling_game.py > ./out/stripped.py
+
+# minify the python source
 # need to compare the efficency of the --obfuscate-import-methods and --obfuscate-builtins options
-pyminifier --outfile=./out/minified.py --obfuscate-classes --obfuscate-functions --obfuscate-variables --replacement-length=1 scrolling_game.py 1>/dev/null
+pyminifier \
+    --outfile=./out/minified.py \
+    --obfuscate-classes \
+    --obfuscate-functions \
+    --obfuscate-variables \
+    --replacement-length=1 \
+    ./out/stripped.py \
+    1>/dev/null
+
+# remove the last line with the link for the pyminifier repo
 sed -i '' -e '$ d' ./out/minified.py
+
+# generate the micropython.hex file
 uflash ./out/minified.py ./out/
